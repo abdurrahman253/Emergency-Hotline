5257c4e714e98dd791896441092545b20968f15c
Abdur Rahman <abdur@example.com>
2025-08-28 06:37:21 +0600

    updated index.html file and created some functionalities in index.js file

--- index.js
+++ index.js
@@ -3,11 +3,37 @@ function getElement(id){
     return element;
 }
 
-// Adding cards on the call history. 
+ 
 
 getElement("card_box").addEventListener("click", function(event){
+
+     // Call Button Functionalities. 
     if(event.target.id === "call_button" ){
+       
        const callButton = event.target;
+
+       // Alert Message.
+       const serviceName = callButton.parentNode.parentNode.children[0].children[1].textContent;
+       const serviceNumber = callButton.parentNode.parentNode.children[1].children[0].textContent;
+       alert("Calling " + serviceName +" "+ serviceNumber )
+       
+       
+
+       // Deducting Coins
+        const coinValue = document.querySelector(".coin_value");
+        const currentCoin = parseInt(coinValue.innerText); 
+
+            if(currentCoin > 0){
+                const newCoinValue = currentCoin - 20;
+                coinValue.innerText = newCoinValue;
+            }
+            else{
+                alert("You don't have enough coins to make a call.You need at least 20 coins to make a call.");
+                return;
+            }
+      
+
+       // Creating Call History Section.
        const helpLineName = callButton.parentNode.parentNode.children[0].children[0].textContent;
        const helpLineNumber = callButton.parentNode.parentNode.children[1].children[0].textContent;
        
@@ -27,17 +53,47 @@ getElement("card_box").addEventListener("click", function(event){
         </div> `
 
         cardContainer.appendChild(newCard) 
+           
+
+
        
     }
 
+
+       
+      // Copy Button Functionalities. 
+    if(event.target.id  === "copy_button"){
+        const copyButton = event.target ;
+        const helpLineNumber = copyButton.parentNode.parentNode.children[1].children[0].textContent;
+        navigator.clipboard.writeText(helpLineNumber);
+        alert("Copied the number: " + helpLineNumber);
+        // Incrementing Copy Counter.
+        const copyCounter = document.querySelector(".copy_counter");
+        const currentCopy = parseInt(copyCounter.innerText);
+        const newCopy = currentCopy + 1;
+        copyCounter.innerText = newCopy; 
+    }
+
+
+    // heart Button Functionalities.
+    if(event.target.id  === "heart_icon"){
+        const heartButton = event.target ;
+        const heartValue = heartButton.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.children[0].children[1].children[0].children[0].textContent
+
+        const heartCounter = document.querySelector(".heart_counter")
+        const newHearValue = parseInt(heartValue) + 1;
+        heartCounter.innerText = newHearValue; 
+      
+        
+
   
   
-})
+}
 
 
 
 
- // Call history clear.
+ // Clear Button Functionalities.
    getElement("clear_button").addEventListener("click", function(){
     const newCard = document.querySelectorAll(".new_card")
 
@@ -48,5 +104,4 @@ getElement("card_box").addEventListener("click", function(event){
   
 
 
-
-
+})
